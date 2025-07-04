364767a963e5bac8a3dba222546950e23392455a
IvoCorrea <ivoaurelio30@gmail.com>
2025-07-04 02:15:32 -0300

    ADD: Controller created

--- src/main/java/com/ivocorrea/investmanager/repository/AtivoRepository.java
+++ src/main/java/com/ivocorrea/investmanager/repository/AtivoRepository.java
@@ -4,5 +4,5 @@ import com.ivocorrea.investmanager.entity.Ativo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AtivoRepository extends JpaRepository<Ativo, Long> {
-
+    Ativo findByTicker(String ticker);
 }
