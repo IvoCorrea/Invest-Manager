b0314e1a87989e6bff87dad5b86a39c623e073c8
IvoCorrea <ivoaurelio30@gmail.com>
2025-07-21 11:39:13 -0300

    refactor(ativos): Ativos deleted

--- src/main/java/com/ivocorrea/investmanager/repository/AtivoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ivocorrea.investmanager.repository;
-
-import com.ivocorrea.investmanager.entity.Ativo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AtivoRepository extends JpaRepository<Ativo, Long> {
-    Ativo findByTicker(String ticker);
-}
