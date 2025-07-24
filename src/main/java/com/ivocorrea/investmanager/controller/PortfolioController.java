503d0c4aff823b71d875e377f3a30d4ddc73396d
IvoCorrea <ivoaurelio30@gmail.com>
2025-07-24 19:15:32 -0300

    feat(portfolio): PortfolioController created

--- src/main/java/com/ivocorrea/investmanager/controller/PortfolioController.java
+++ src/main/java/com/ivocorrea/investmanager/controller/PortfolioController.java
@@ -1,32 +1,60 @@
 package com.ivocorrea.investmanager.controller;
 
+import com.ivocorrea.investmanager.dto.AddAssetDTO;
+import com.ivocorrea.investmanager.dto.CreatePortfolioDTO;
 import com.ivocorrea.investmanager.entity.Asset;
 import com.ivocorrea.investmanager.entity.Portfolio;
+import com.ivocorrea.investmanager.service.PortfolioService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/portfolio")
 public class PortfolioController {
+    private final PortfolioService portfolioService;
+
+    public PortfolioController(PortfolioService portfolioService) {
+        this.portfolioService = portfolioService;
+    }
 
     @PostMapping
-    private ResponseEntity<Portfolio> createPortfolio(@RequestBody String portfolio) {
-        return null;
+    private ResponseEntity<Portfolio> createPortfolio(@RequestBody CreatePortfolioDTO portfolioDTO) {
+        UUID createdPortfolio = portfolioService.createPortfolio(portfolioDTO);
+        return ResponseEntity.created(URI.create("/portfolio/" + createdPortfolio.toString())).build();
     }
 
     @GetMapping("/{portfolioId}")
-    public ResponseEntity<Portfolio> getPortfolioById(@PathVariable String portfolioId) {
-        return null;
+    public ResponseEntity<Optional<Portfolio>> getPortfolioById(@PathVariable String portfolioId) {
+        Optional<Portfolio> portfolio = portfolioService.getPortfolioById(portfolioId);
+        return ResponseEntity.ok(portfolio);
     }
 
-    @PostMapping("/portfolio/{assetId}/asset")
-    public ResponseEntity<Portfolio> addAssetToPortfolio(@PathVariable String portfolioId) {
-        return null;
+    @DeleteMapping("/{portfolioId}")
+    public ResponseEntity<Void> deletePortfolio(@PathVariable String portfolioId) {
+        portfolioService.deletePortfolio(portfolioId);
+        return ResponseEntity.noContent().build();
     }
 
-    @GetMapping("/portfolio/{assetId}/asset")
-    public ResponseEntity<Asset> getAsset(@PathVariable String assetId) {
-        return null;
+    @PatchMapping("/{portfolioId}/asset")
+    public ResponseEntity<Portfolio> addAssetToPortfolio(
+            @RequestBody AddAssetDTO addAssetDTO,
+            @PathVariable String portfolioId
+    ) {
+        Portfolio portfolio = portfolioService.addAssetToPortfolio(addAssetDTO, portfolioId);
+        return ResponseEntity.ok(portfolio);
     }
 
+    @DeleteMapping("/{portfolioId}/asset/{assetId}")
+    public ResponseEntity<Void> deleteAssetInPortfolio(
+            @PathVariable String portfolioId,
+            @PathVariable String assetId
+    ) {
+        portfolioService.deleteAssetInPortfolio(portfolioId, assetId);
+        return ResponseEntity.noContent().build();
+    }
 }
