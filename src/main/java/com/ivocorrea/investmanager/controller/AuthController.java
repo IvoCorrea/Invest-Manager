1f0eb33a8724d5849393922f8b56712cd6a32f1a
IvoCorrea <ivoaurelio30@gmail.com>
2025-08-06 23:04:36 -0300

    feat: implement user registration and login with token generation

---
+++ src/main/java/com/ivocorrea/investmanager/controller/AuthController.java
@@ -0,0 +1,40 @@
+package com.ivocorrea.investmanager.controller;
+
+import com.ivocorrea.investmanager.dto.CreateUserDto;
+import com.ivocorrea.investmanager.dto.LoginRequestDTO;
+import com.ivocorrea.investmanager.dto.LoginResponseDTO;
+import com.ivocorrea.investmanager.entity.User;
+import com.ivocorrea.investmanager.service.AuthService;
+import com.ivocorrea.investmanager.service.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    private final UserService userService;
+    private final AuthService authService;
+
+    public AuthController(UserService userService, AuthService authService) {
+        this.userService = userService;
+        this.authService = authService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<User> registerUser(@RequestBody CreateUserDto userDto) {
+        UUID userId = userService.createUser(userDto);
+        return ResponseEntity.created(URI.create("/user/" + userId.toString())).build();
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponseDTO> loginAuth(@RequestBody LoginRequestDTO requestDTO) {
+        return ResponseEntity.ok(authService.login(requestDTO));
+    }
+}
